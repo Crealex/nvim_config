29ffe48522c120a1c8a7a47721d5f306a76cfa5c
Crealex <Alexandre.tomasi80@gmail.com>
2026-02-18 11:50:21 +0100

    added option to the cursor on neoivde

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -38,4 +38,6 @@ if vim.g.neovide then
 	vim.g.neovide_opacity = 0.3
 	vim.g.neovide_background_color = "#0f1117" .. string.format("%x", math.floor(255 * 0.3)) --je sais pas trop a quoi sert cette ligne, apparment pour de l'opti
 	vim.g.neovide_scale_factor = 1.0
+	vim.g.neovide_cursor_vfx_mode = { "railgun", "ripple" }
+	vim.g.neovide_cursor_vfx_particle_density = 1
 end
